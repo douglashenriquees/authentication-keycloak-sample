52b26783a5f47e2ef59a1ab89bea5fdba1e53ff4
douglas.castro <douglas.castro@jazztech.com.br>
2023-09-26 17:25:14 -0300

    initial commit

--- AuthKeyCloakSample/Program.cs
+++ AuthKeyCloakSample/Program.cs
@@ -1,7 +1,40 @@
+using Keycloak.AuthServices.Authentication;
+using Keycloak.AuthServices.Authorization;
+using Keycloak.AuthServices.Sdk.Admin;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var authenticationOptions = builder
+	.Configuration
+	.GetSection(KeycloakAuthenticationOptions.Section)
+	.Get<KeycloakAuthenticationOptions>();
+
+var authorizationOptions = builder
+	.Configuration
+	.GetSection(KeycloakProtectionClientOptions.Section)
+	.Get<KeycloakProtectionClientOptions>();
+
+var adminClientOptions = builder
+	.Configuration
+	.GetSection(KeycloakAdminClientOptions.Section)
+	.Get<KeycloakAdminClientOptions>();
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddKeycloakAuthentication(
+	authenticationOptions ?? throw new ArgumentNullException(nameof(authenticationOptions)));
+
+builder.Services.AddKeycloakAuthorization(
+	authorizationOptions ?? throw new ArgumentNullException(nameof(authorizationOptions)));
+
+builder.Services.AddKeycloakAdminHttpClient(
+	adminClientOptions ?? throw new ArgumentNullException(nameof(adminClientOptions)));
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/", () => "Hello World!").RequireAuthorization();
 
 app.Run();
--- README.md
+++ README.md
@@ -1,5 +1,18 @@
+# Running Container Keycloak
+
+* docker network create -d bridge auth-sample
+* docker run -p 9090:8080 --network=auth-sample --name keycloak-local -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:latest start-dev
+* configure to increase token expiration time:
+  * realm master
+  * realm settings
+  * access token lifespan: 30 minutes
+
 # Creating the Project
 
 * dotnet new gitignore
 * dotnet new web -o AuthKeyCloakSample --no-https true
 * cd AuthSample and execute the debug tool for create the assets
+* dotnet add package IdentityModel.AspNetCore
+* dotnet add package Keycloak.AuthServices.Authentication
+* dotnet add package Keycloak.AuthServices.Authorization
+* dotnet add package Refit.HttpClientFactory
